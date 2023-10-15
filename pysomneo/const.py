65897e2a2149c4e1fa5bf2914e60d8d124fecdd4
theneweinstein <34983911+theneweinstein@users.noreply.github.com>
2023-10-15 16:32:26 +0200

    Fix namen days

--- pysomneo/const.py
+++ pysomneo/const.py
@@ -15,6 +15,6 @@ SOUND_CHANNEL_DUSK = {'soft rain': '1', 'ocean waves': '2', 'under water': '3', 
 SOURCES = {'AUX': 'aux', 'FM 1': 1,'FM 2': 2,'FM 3': 3,'FM 4': 4,'FM 5': 5, 'Other': 'other'}
 
 DAYS = {0: 'tomorrow', 2: 'mon', 4: 'tue', 8: 'wed', 16: 'thu', 32: 'fri', 64: 'sat', 128: 'sun'}
-DAYS_TYPE = {0: 'tomorrow', 62: 'weekdays', 192: 'weekend', 254: 'everyday'}
+DAYS_TYPE = {0: 'tomorrow', 62: 'workdays', 192: 'weekend', 254: 'daily'}
 
 STATUS = {1: 'off', 2321: 'snooze', 2309: 'wake-up', 2817: 'on'}

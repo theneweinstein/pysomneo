d7056b3aa057193c12d887db388b8fc9f35557c1
theneweinstein <34983911+theneweinstein@users.noreply.github.com>
2024-01-01 19:58:35 +0100

    Update const.py

--- pysomneo/const.py
+++ pysomneo/const.py
@@ -13,7 +13,7 @@ SOUND_SOURCE_DUSK = {'radio': 'fmr', 'off': 'off','dusk': 'dus'}
 SOUND_CHANNEL_DUSK = {'soft rain': '1', 'ocean waves': '2', 'under water': '3', 'summer lake': '4'}
 SOUND_DUSK = {'soft rain': '1', 'ocean waves': '2', 'under water': '3', 'summer lake': '4', 'fm 1': 1, 'fm 2': 2, 'fm 3': 3, 'fm 4': 4, 'fm 5': 5, 'off': 'off'}
 
-SOURCES = {'AUX': 'aux', 'fm 1': 1,'fm 2': 2,'fm 3': 3,'fm 4': 4,'fm 5': 5, 'other': 'other'}
+SOURCES = {'AUX': 'aux', 'FM 1': 1,'FM 2': 2,'FM 3': 3,'FM 4': 4,'FM 5': 5, 'other': 'other'}
 
 DAYS = {0: 'tomorrow', 2: 'mon', 4: 'tue', 8: 'wed', 16: 'thu', 32: 'fri', 64: 'sat', 128: 'sun'}
 DAYS_TYPE = {0: 'tomorrow', 62: 'workdays', 192: 'weekend', 254: 'daily'}
